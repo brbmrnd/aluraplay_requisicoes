04ffca5957bd0bb771fa13c6bdfb086dd455c67b
bárbara miranda <barbaramntmiranda@gmail.com>
2024-06-29 16:44:44 -0300

    add POST request method for adding videos

--- js/conectApi.js
+++ js/conectApi.js
@@ -5,6 +5,24 @@ async function videoList() {
     return convertedConection;
 }
 
+async function addVideo(title, description, url, image) {
+    const conection = await fetch('http://localhost:3000/videos',
+        {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({
+                titulo: title,
+                descricao: `${description} mil visualizações`,
+                url: url,
+                imagem: image
+            })
+
+        });
+    const convertedConection = conection.json();
+    return convertedConection;
+}
+
 export const conectApi = {
-    videoList
+    videoList,
+    addVideo
 }
