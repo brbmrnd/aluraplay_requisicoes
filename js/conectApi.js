4e823063a8c33a0a2eed9b5b12dccbfda6fe12a0
bárbara miranda <barbaramntmiranda@gmail.com>
2024-06-29 13:06:46 -0300

    add api conection

---
+++ js/conectApi.js
@@ -0,0 +1,10 @@
+async function videoList() {
+    const conection = await fetch('http://localhost:3000/videos');
+    const convertedConection = conection.json();
+
+    return convertedConection;
+}
+
+export const conectionApi = {
+    videoList
+}
